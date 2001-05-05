ac3509e4f52c5454aadb843dcd34e5eabdceff14
jbj <jbj>
2001-05-05 19:27:27 +0000

    - enough lclint annotations and fiddles already.

--- system.h
+++ system.h
@@ -1,48 +1,13 @@
-/**
- * \file popt/system.h
- */
-
 #ifdef HAVE_CONFIG_H
 #include "config.h"
 #endif
 
-
-
-#if defined (__GLIBC__) && defined(__LCLINT__)
-/*@-declundef@*/
-/*@unchecked@*/
-extern __const __int32_t *__ctype_tolower;
-/*@unchecked@*/
-extern __const __int32_t *__ctype_toupper;
-/*@=declundef@*/
-#endif
-
 #include <ctype.h>
-
-
-#ifdef __GNUC__
-# undef alloca
-# define alloca __builtin_alloca
-#else
-# ifdef HAVE_ALLOCA_H
-#  include <alloca.h>
-# else
-#  ifndef _AIX
-/* AIX alloca decl has to be the first thing in the file, bletch! */
-char *alloca ();
-#  endif
-# endif
-#endif
-
-
-/* XXX isspace(3) has i18n encoding signednesss issues on Solaris. */
-#define	_isspaceptr(_chp)	isspace((int)(*(unsigned char *)(_chp)))
-
 #include <errno.h>
 #include <fcntl.h>
 #include <limits.h>
 
-#ifdef HAVE_MCHECK_H
+#if HAVE_MCHECK_H 
 #include <mcheck.h>
 #endif
 
@@ -50,77 +15,9 @@ char *alloca ();
 #include <stdlib.h>
 #include <string.h>
 
-#if defined(HAVE_UNISTD_H) && !defined(__LCLINT__)
+#if HAVE_UNISTD_H
 #include <unistd.h>
 #endif
-
-#ifdef _MSC_VER
-#  define inline __inline
-#define _CRT_SECURE_NO_WARNINGS 1
-#include <io.h>
-#include <malloc.h>
-#define F_OK 0
-#define R_OK 4
-#define W_OK 2
-#define X_OK 1
-
-#define S_IRWXG 00000070
-#define S_IRWXO 00000007
-#define S_ISUID 00040000
-#define S_ISGID 00020000
-#define S_ISVTX 00010000
-
-
-/* CHECKME */
-#define S_IWGRP 00000020
-#define S_IWOTH 00000002
-
-#define S_ISREG(m) ((m)&_S_IFREG)
-
-
-/* Documentation claims these are in limits.h, but it seems to be a lie */
-/* Microsoft documentation http://msdn.microsoft.com/en-us/library/k15zsh48.aspx */
-
-#if !defined(FLT_MIN)
-#define FLT_MIN 1.175494351e-38F
-#endif
-
-#if !defined(FLT_MAX)
-#define FLT_MAX 3.402823466e+38F
-#endif
-
-typedef int uid_t;
-
-#ifdef _WIN64
-typedef __int64 ssize_t;
-#else
-typedef int ssize_t;
-#endif /* _WIN64 */
-
-#define srandom srand
-#define random rand
-#define access _access
-#define strdup _strdup
-#define execvp _execvp
-#define strtoll _strtoi64
-#define read _read
-#define open _open
-#define close _close
-#define lseek _lseek
-
-/* Pretend to be root to replace these */
-inline int setuid(int x) { return 1; }
-inline int getuid(void) { return 0; }
-
-inline int seteuid(int x) { return 1; }
-inline int geteuid(void) { return 0; }
-
-inline int setgid(int x) { return 1; }
-inline int getgid(void) { return 0; }
-
-inline int setegid(int x) { return 1; }
-
-#endif /* _MSC_VER */
 
 #ifdef __NeXT
 /* access macros are not declared in non posix mode in unistd.h -
@@ -128,66 +25,33 @@ inline int setegid(int x) { return 1; }
 #include <libc.h>
 #endif
 
-
-/*@-incondefs@*/
-/*@mayexit@*/ /*@only@*/ /*@out@*/ /*@unused@*/
-void * xmalloc (size_t size)
-	/*@globals errno @*/
-	/*@ensures maxSet(result) == (size - 1) @*/
-	/*@modifies errno @*/;
-
-/*@mayexit@*/ /*@only@*/ /*@unused@*/
-void * xcalloc (size_t nmemb, size_t size)
-	/*@ensures maxSet(result) == (nmemb - 1) @*/
-	/*@*/;
-
-/*@mayexit@*/ /*@only@*/ /*@unused@*/
-void * xrealloc (/*@null@*/ /*@only@*/ void * ptr, size_t size)
-	/*@ensures maxSet(result) == (size - 1) @*/
-	/*@modifies *ptr @*/;
-
-/*@mayexit@*/ /*@only@*/ /*@unused@*/
-char * xstrdup (const char *str)
-	/*@*/;
-/*@=incondefs@*/
-
-#if !defined(HAVE_STPCPY)
-/* Copy SRC to DEST, returning the address of the terminating '\0' in DEST.  */
-static inline char * stpcpy (char *dest, const char * src) {
-    register char *d = dest;
-    register const char *s = src;
-
-    do
-	*d++ = *s;
-    while (*s++ != '\0');
-    return d - 1;
-}
+/* AIX requires this to be the first thing in the file.  */ 
+#ifndef __GNUC__
+# if HAVE_ALLOCA_H
+#  include <alloca.h>
+# else
+#  ifdef _AIX
+#pragma alloca
+#  else
+#   ifndef alloca /* predefined by HP cc +Olibcalls */
+char *alloca ();
+#   endif
+#  endif
+# endif
+#elif defined(__GNUC__) && defined(__STRICT_ANSI__)
+#define alloca __builtin_alloca
 #endif
 
-/* Memory allocation via macro defs to get meaningful locations from mtrace() */
-#if defined(HAVE_MCHECK_H) && defined(__GNUC__)
+/*@only@*/ char * xstrdup (const char *str);
+
+#if !defined(__LCLINT__)
+#if HAVE_MCHECK_H && defined(__GNUC__)
 #define	vmefail()	(fprintf(stderr, "virtual memory exhausted.\n"), exit(EXIT_FAILURE), NULL)
-#define	xmalloc(_size) 		(malloc(_size) ? : vmefail())
-#define	xcalloc(_nmemb, _size)	(calloc((_nmemb), (_size)) ? : vmefail())
-#define	xrealloc(_ptr, _size)	(realloc((_ptr), (_size)) ? : vmefail())
 #define xstrdup(_str)   (strcpy((malloc(strlen(_str)+1) ? : vmefail()), (_str)))
 #else
-#define	xmalloc(_size) 		malloc(_size)
-#define	xcalloc(_nmemb, _size)	calloc((_nmemb), (_size))
-#define	xrealloc(_ptr, _size)	realloc((_ptr), (_size))
 #define	xstrdup(_str)	strdup(_str)
-#endif  /* defined(HAVE_MCHECK_H) && defined(__GNUC__) */
+#endif  /* HAVE_MCHECK_H && defined(__GNUC__) */
+#endif /* !__LCLINT__ */
 
-#if defined(HAVE___SECURE_GETENV) && !defined(__LCLINT__)
-#define	getenv(_s)	__secure_getenv(_s)
-#endif
 
-#if !defined(__GNUC__) && !defined(__attribute__)
-#define __attribute__(x)
-#endif
-#define UNUSED(x) x __attribute__((__unused__))
-
-/* Include configmake.h autogenerated from Makefile.am */
-
-#include "configmake.h"
 #include "popt.h"
--- test2.c
+++ test2.c
@@ -1,12 +1,12 @@
 /*
-    Popt Library Test Program Number Too
-
+    Popt Library Test Program Number Too 
+    
     --> "a real world test of popt bugs" <--
 
     Copyright (C) 1999 US Interactive, Inc.
 
     This program can be used under the GPL or LGPL at your
-    whim as long as this Copyright remains attached.
+    whim as long as this Copyright remains attached.  
 */
 
 #include "system.h"
@@ -26,7 +26,7 @@ char *contentPath		= NULL;
 char *dbPassword		= NULL;
 char *dbUserName		= NULL;
 
-char *rcfile = "test-poptrc";
+char *rcfile = "createuser-defaults";
 char *username=NULL;
 
 char *password			= NULL;
@@ -45,7 +45,7 @@ char *dayphone			= NULL;
 char *fax			= NULL;
 
 
-int
+int 
 main(int argc, const char ** argv) {
 
     poptContext optCon;   /* context for parsing command-line options */
@@ -123,26 +123,12 @@ main(int argc, const char ** argv) {
     optionsTable[1].arg = databaseOptionsTable;
     optionsTable[2].arg = userOptionsTable;
 
-#if defined(HAVE_MCHECK_H) && defined(HAVE_MTRACE)
+#if HAVE_MCHECK_H && HAVE_MTRACE
     mtrace();   /* Trace malloc only if MALLOC_TRACE=mtrace-output-file. */
 #endif
 
-    optCon = poptGetContext("test2", argc, argv, optionsTable, 0);
-#ifdef HAVE_STDLIB_H
-    rcfile = getenv ("testpoptrc");
-    if (rcfile != NULL ) { 
-    (void) poptReadConfigFile(optCon, rcfile );
-    }
-    else {
-    (void) poptReadConfigFile(optCon, "./test-poptrc");
-/* XXXX: make distcheck succed : test2 is in popt-<version>/_build */
-    (void) poptReadConfigFile(optCon, "../../test-poptrc");
-    }
-#else
-    (void) poptReadConfigFile(optCon, "./test-poptrc");
-/* XXXX: make distcheck succed : test2 is in popt-<version>/_build */
-    (void) poptReadConfigFile(optCon, "../../test-poptrc");
-#endif
+    optCon = poptGetContext("createuser", argc, argv, optionsTable, 0);
+    poptReadConfigFile(optCon, rcfile );
 
     /* although there are no options to be parsed, check for --help */
     poptGetNextOpt(optCon);
