6f996ace7cb0af7eefe0130905f0d6e192901dd9
jbj <jbj>
2002-01-18 23:30:25 +0000

    Sync with rpm-4.1.

--- poptparse.c
+++ poptparse.c
@@ -32,10 +32,12 @@ int poptDupArgv(int argc, const char **argv,
     argv2 = (void *) dst;
     dst += (argc + 1) * sizeof(*argv);
 
+    /*@-branchstate@*/
     for (i = 0; i < argc; i++) {
 	argv2[i] = dst;
 	dst += strlen(strcpy(dst, argv[i])) + 1;
     }
+    /*@=branchstate@*/
     argv2[argc] = NULL;
 
     if (argvPtr) {
@@ -90,7 +92,7 @@ int poptParseArgvString(const char * s, int * argcPtr, const char *** argvPtr)
 	  case '"':
 	  case '\'':
 	    quote = *src;
-	    break;
+	    /*@switchbreak@*/ break;
 	  case '\\':
 	    src++;
 	    if (!*src) {
@@ -100,7 +102,7 @@ int poptParseArgvString(const char * s, int * argcPtr, const char *** argvPtr)
 	    /*@fallthrough@*/
 	  default:
 	    *buf++ = *src;
-	    break;
+	    /*@switchbreak@*/ break;
 	}
     }
 
